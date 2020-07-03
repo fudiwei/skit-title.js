0572b88f166b9ac07cc982984aa5313fd676fc3f
Fu Diwei <fudiwei@sina.com>
2020-07-03 13:25:58 +0800

    fix: TS declare error

--- types/index.d.ts
+++ types/index.d.ts
@@ -1,30 +1,34 @@
-export default STEP.Title.default;
+export default STEP.Title.$$;
 
 export namespace STEP.Title {
-    export default {
+    export interface Title {}
+
+    export interface TitleConstructor<T extends Title = Title> {
         /**
          * 设置标题。
          * @param {String} title 标题。
          */
-        setTitle(title: string): void;,
+        setTitle(title: string) : void;
 
         /**
          * 设置标题前缀。
          * @param {String} prefix 前缀。
          */
-        setPrefix(prefix: string): void;,
+        setPrefix(prefix: string): void;
 
         /**
          * 设置标题后缀。
          * @param {String} suffix 后缀。
          */
-        setSuffix(suffix: string): void;,
+        setSuffix(suffix: string): void;
 
         /**
          * 设置兼容 iOS/MacOS 时的加载的内容路径，默认值："/favicon.ico"。
          * @param {String} src 加载的内容路径。
          * @param {Boolean} [flush] （可选）是否清空前缀和后缀，默认值 false。
          */
-        withHackSource(src: string, flush: boolean = false): void;
+        withHackSource(src: string, flush?: boolean): void;
     }
+
+    export const $$: TitleConstructor;
 }
