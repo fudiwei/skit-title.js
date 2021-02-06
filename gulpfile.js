0bc3112ca94839c45b8d2ead980731f89d64ed8a
Fu Diwei <fudiwei@sina.com>
2021-02-06 10:47:46 +0800

    style: prettier config

--- gulpfile.js
+++ gulpfile.js
@@ -11,53 +11,55 @@ const gulp$babel = require('gulp-babel'),
 const ARGS_OUTPUTFILES = gulp$util.env.outputFiles || 'both'; // 指示输出文件类型。可取值 both / min
 
 gulp.task('clean', () => {
-	return gulp
-		.src(['dist/**/*.js', 'dist/**/*.js.map'], {
+    return gulp
+        .src(['dist/**/*.js', 'dist/**/*.js.map'], {
             read: false
         })
-        .pipe(gulp$rimraf({
-            force: true
-        }));
+        .pipe(
+            gulp$rimraf({
+                force: true
+            })
+        );
 });
 
 gulp.task('eslint', () => {
-	return gulp
-		.src(['src/**/*.js'])
-        .pipe(gulp$eslint())
-        .pipe(gulp$eslint.format())
-        .pipe(gulp$eslint.failAfterError());
+    return gulp.src(['src/**/*.js']).pipe(gulp$eslint()).pipe(gulp$eslint.format()).pipe(gulp$eslint.failAfterError());
 });
 
-gulp.task('dist', gulp.series('clean', () => {
-    const files = ['src/**/*.js'];
-    const babelOptions = {
-        presets: [
-            [
-                '@babel/preset-env', {
-                    modules: false
-                }
+gulp.task(
+    'dist',
+    gulp.series('clean', () => {
+        const files = ['src/**/*.js'];
+        const babelOptions = {
+            presets: [
+                [
+                    '@babel/preset-env',
+                    {
+                        modules: false
+                    }
+                ]
             ]
-        ]
-    };
+        };
 
-    if ('both' === ARGS_OUTPUTFILES) {
-        gulp.src(files)
-            .pipe(gulp$concat('index.js'))
+        if ('both' === ARGS_OUTPUTFILES) {
+            gulp.src(files)
+                .pipe(gulp$concat('index.js'))
+                .pipe(gulp$sourcemaps.init())
+                .pipe(gulp$babel(babelOptions))
+                .pipe(gulp$sourcemaps.write('.'))
+                .pipe(gulp.dest('dist'));
+        }
+
+        return gulp
+            .src(files)
+            .pipe(gulp$concat('index.min.js'))
+            .pipe(gulp$strip())
             .pipe(gulp$sourcemaps.init())
             .pipe(gulp$babel(babelOptions))
+            .pipe(gulp$uglify())
             .pipe(gulp$sourcemaps.write('.'))
             .pipe(gulp.dest('dist'));
-    }
-
-	return gulp
-		.src(files)
-        .pipe(gulp$concat('index.min.js'))
-        .pipe(gulp$strip())
-        .pipe(gulp$sourcemaps.init())
-        .pipe(gulp$babel(babelOptions))
-        .pipe(gulp$uglify())
-        .pipe(gulp$sourcemaps.write('.'))
-        .pipe(gulp.dest('dist'));
-}));
+    })
+);
 
 gulp.task('default', gulp.series('clean', 'eslint', 'dist'));
--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-(function moduleDefinition(root, factory) {
+(function moduleDefinition (root, factory) {
     const $ = '$$title';
 
     if ('object' === typeof exports && 'object' === typeof module) {
@@ -8,33 +8,31 @@
     } else if (root) {
         root[$] = factory();
     }
-})(this, (() => {
+})(this, () => {
     'use strict';
     const $ = {};
 
     let myDocument = document || (window && window.document) || {};
     let myNavigator = navigator || (window && window.navigator) || {};
-    let valTitle = myDocument.title || '',
-        valPrefix = '',
-        valSuffix = '',
-        valHackSrc = '/favicon.ico';
+    let valTitle = myDocument.title || '';
+    let valPrefix = '';
+    let valSuffix = '';
+    let valHackSrc = '/favicon.ico';
 
-    const requireHacked = !!myNavigator.userAgent && 
-        !!myNavigator.userAgent.match && 
-        !!myNavigator.userAgent.match(/Safari|AppleWebKit|iPhone|iPod|iPad/);
+    const requireHacked = !!myNavigator.userAgent && !!myNavigator.userAgent.match && !!myNavigator.userAgent.match(/Safari|AppleWebKit|iPhone|iPod|iPad/);
 
-    function resetDocumentTitle() {
+    function resetDocumentTitle () {
         document.title = valPrefix + valTitle + valSuffix;
 
         if (requireHacked) {
             let hackFrame = myDocument.createElement && myDocument.createElement('iframe');
-            let callback = function() {
+            let callback = function () {
                 setTimeout(() => {
                     myDocument.body && myDocument.body.removeChild && myDocument.body.removeChild(hackFrame);
                     hackFrame = null;
                     callback = null;
                 }, 50);
-            }
+            };
 
             if (!!hackFrame) {
                 hackFrame.setAttribute('style', 'display: none!important; visibility: hidden!important; width: 0; height: 0; z-index: -9999;');
@@ -47,11 +45,7 @@
         }
     }
 
-    $.setTitle = (title, flush = false) => {
-        if (title === null || title === void 0) {
-            title = '';
-        }
-
+    $.setTitle = (title = '', flush = false) => {
         if (flush) {
             valPrefix = '';
             valSuffix = '';
@@ -61,31 +55,19 @@
         resetDocumentTitle();
     };
 
-    $.setPrefix = (prefix) => {
-        if (prefix === null || prefix === void 0) {
-            prefix = '';
-        }
-
+    $.setPrefix = (prefix = '') => {
         valPrefix = prefix + '';
         resetDocumentTitle();
     };
 
-    $.setSuffix = (suffix) => {
-        if (suffix === null || suffix === void 0) {
-            suffix = '';
-        }
-
+    $.setSuffix = (suffix = '') => {
         valSuffix = suffix + '';
         resetDocumentTitle();
     };
 
-    $.withHackSource = (src) => {
-        if (src === null || src === void 0) {
-            src = '/favicon.ico';
-        }
-
+    $.withHackSource = (src = '/favicon.ico') => {
         valHackSrc = src;
     };
 
     return $;
-}));
+});
